a2f75d753243fc29028d87224c4e7049be54e930
yzy881206 <75056595+yzy881206@users.noreply.github.com>
2021-04-04 22:21:51 +0800

    2020/4/4

---
+++ homework/Day_003_HW.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## [作業目標]
+# 持續接觸有關機器學習的相關專案與最新技術
+
+# ## [作業重點]
+# 透過觀察頂尖公司的機器學習文章，來了解各公司是怎麼應用機器學習在實際的專案上
+
+# ## [作業]
+# 今天的作業希望大家能夠看看全球機器學習巨頭們在做的機器學習專案。以 google 為例，下圖是 Google 內部專案使用機器學習的數量，隨著時間進展，現在早已超過 2000 個專案在使用機器學習。
+
+# ![image](https://cdn-images-1.medium.com/max/800/1*U_L8qI8RmYS-MOBrYvXhSA.png)
+
+# 底下幫同學整理幾間知名企業的 blog 或機器學習網站 (自行搜尋也可)，這些網站都會整理最新的機器學習專案或者是技術文章，請挑選一篇文章閱讀並試著回答
+# 1. 專案的目標？ (要解決什麼問題）
+# 2. 使用的技術是？ (只需知道名稱即可，例如：使用 CNN 卷積神經網路做影像分類)
+# 3. 資料來源？ 
+
+# - [Google AI blog](https://ai.googleblog.com/)
+# - [Facebook Research blog](https://research.fb.com/blog/)
+# - [Apple machine learning journal](https://machinelearning.apple.com/)
+# - [機器之心](https://www.jiqizhixin.com/)
+# - [雷鋒網](http://www.leiphone.com/category/ai)
+
+# In[ ]:
+
+
+所選文章:https://www.jiqizhixin.com/articles/2021-04-02-8
+
+裡面提到美國會辦名為美洲杯帆船賽的競技帆船比賽，其中的一隊利用強化學習訓練了一位AI水手來在各種模擬環境下測試不同的船隻設計表現
+如此一來便不用實際把船設計出來便可以測試其性能表現如何。
+
+1.如何在能有效測試船體設計的表現情況下降低設計船體的成本
+2.強化學習讓電腦學習如何成為合格的水手(讓電腦學會操控帆船)
+3.環境變數由電腦模擬，帆船設計由人類設計師提供
+
